b0747b779c4f7802175373008de946f1614b284a
Vinzent <vinzent03@protonmail.com>
2022-03-13 12:51:23 +0100

    fix: don't encode the whole uri, instead just the values

--- src/main.ts
+++ src/main.ts
@@ -716,7 +716,7 @@ export default class AdvancedURI extends Plugin {
 
     async copyURI(parameters: Parameters) {
         const uri = await this.generateURI(parameters);
-        await this.copyText(encodeURI(uri));
+        await this.copyText(uri);
 
         new Notice("Advanced URI copied to your clipboard");
     }
